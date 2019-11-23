79ce8285151100f59f5ebb927fa29fc59f046b04
Alban Fichet <alban.fichet@inria.fr>
2019-11-23 18:27:58 +0100

    Removing deprecated code and disabling MIME update

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gnome-openexr-thumbnailer"
-
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
@@ -17,4 +15,4 @@ which gnome-autogen.sh || {
 	exit 1
 }
 
-REQUIRED_PKG_CONFIG_VERSION=0.17.1 REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
+REQUIRED_PKG_CONFIG_VERSION=0.17.1 REQUIRED_AUTOMAKE_VERSION=1.9 . gnome-autogen.sh
